05ed64107e2d53c6fd2bcd03fdbd336eb718e622
Jean-Paul Bassil <Jean-Paul Bassil>
2024-06-15 23:12:40 +0300

    chore: Update npm dependencies and add Next UI components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import Header from './components/Header'
 import { NextUIProvider } from '@nextui-org/system'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -19,9 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NextUIProvider>
-          <Header>{children}</Header>
-        </NextUIProvider>
+        <NextUIProvider>{children}</NextUIProvider>
       </body>
     </html>
   )
