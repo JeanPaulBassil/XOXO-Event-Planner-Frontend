3f7e73d4ffa836abaddb965c999ea1470a884295
Jean-Paul Bassil <Jean-Paul Bassil>
2024-06-15 20:24:05 +0300

    chore: Update npm dependencies and add Next UI components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,30 +1,32 @@
-import type { Config } from "tailwindcss";
+import { nextui } from '@nextui-org/theme'
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
     colors: {
-      primary: {
+      light: {
         50: '#FFFFFF',
         100: '#F5F2F2',
         200: '#E5D9D9',
         300: '#9E7878',
         400: '#171212',
-        red: '#C73838'
+        500: '#C73838',
       },
-    }
+    },
   },
-  plugins: [],
-};
-export default config;
+  darkMode: 'class',
+  plugins: [nextui()],
+}
+export default config
