42cdc5bfe427505236e8466b7403eb1172bda737
Georges5091 <gnehme31@gmail.com>
2024-10-03 08:44:20 +0300

    fix(edit-event): updated description display in cake table

--- src/app/(home)/events/[id]/edit/page.tsx
+++ src/app/(home)/events/[id]/edit/page.tsx
@@ -1518,7 +1518,7 @@ const editCake = (cakeName: string, updatedCake: Partial<CakeInTable>) => {
       case 'description':
         return (
           <div>
-            <p className="text-bold">{cake.description}</p>
+            <p className="text-bold break-words max-w-[255px]">{cake.description}</p>
           </div>
         );
         case 'type':
